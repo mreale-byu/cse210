f2eaafdd6bfcc550929a188596b972adb0be20c4
Mauricio Reale <mauricio@protonsistemas.com.br>
2026-06-03 16:36:27 -0300

    W05 Learning Activity: Inheritance

---
+++ week05/Homework/Assignment.cs
@@ -0,0 +1,24 @@
+public class Assignment
+{
+    private readonly string _studentName;
+    private readonly string _topic;
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        return $"{_studentName} - {_topic}";
+    }
+
+    protected string GetStudentName()
+    {
+        return _studentName;
+    }
+
+
+
+}
---
+++ week05/Homework/MathAssignment.cs
@@ -0,0 +1,20 @@
+public class MathAssignment : Assignment
+{
+    private readonly string _textbookSection;
+    private readonly string _problems;
+
+    public MathAssignment(string studentName, 
+                          string topic, 
+                          string textbookSection, 
+                          string problems)
+        : base(studentName, topic)
+    {
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+  
+    public string GetHomeworkList()
+    {
+        return $"Section {_textbookSection} Problems {_problems}";
+    }
+}
